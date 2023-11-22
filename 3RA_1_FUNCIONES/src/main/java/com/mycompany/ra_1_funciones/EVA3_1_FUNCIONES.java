0ee47f538485cb6a95b74c0475dc924dc6c92eec
Venzorprog72 <143563081+Venzorprog72@users.noreply.github.com>
2023-11-22 17:49:46 -0600

    Add files via upload

---
+++ 3RA_1_FUNCIONES/src/main/java/com/mycompany/ra_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ra_1_funciones;
+
+/**
+ *
+ * @author Alfredo Venzor
+ */
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+        //llamada a funcion o procedimiento
+        imprimirMensaje("Hola mundo!!",5);
+        
+        
+       
+    }
+    
+    public static  void  imprimirMensaje(String mensaje,int cant){
+        for (int i = 0; i < cant; i++) {
+            System.out.println("mensaje"); 
+        }
+      
+        
+    }
+            
+            
+        
+}
